dbadc33f726f43fa8a81797483868b4c946d43f9
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-05-22 18:15:52 +0300

    Updates

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,7 +1,24 @@
+import meat from '../uploads/img/menu/meat.jpg';
+import coffee from '../uploads/img/menu/coffee.jpg';
+
 const Menu = () => {
     return (
         <div className="menu">
-            <h2>Menu</h2>
+            <div className="meat-img">
+                <img src={meat} alt="meat" />
+            </div>
+            <div className="menu-elements">
+                <h1>Menu</h1>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <button>Learn More</button>
+            </div>
+            <div className="coffee-img">
+                <img src={coffee} alt="coffee" />
+            </div>
         </div>
     );
 }
