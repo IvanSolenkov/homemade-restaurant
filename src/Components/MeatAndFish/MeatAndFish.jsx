46cab9d0de3d741a58a17eb9c34fb75107ae574d
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-05-29 14:36:01 +0300

    Updates

--- src/Components/MeatAndFish/MeatAndFish.jsx
+++ src/Components/MeatAndFish/MeatAndFish.jsx
@@ -1,7 +1,34 @@
+import meat from '../uploads/img/meat-fish/meat.jpeg';
+import fish from '../uploads/img/meat-fish/fish.jpeg';
+
 const MeatAndFish = () => {
     return (
         <div className="meat-fish">
-            <h2>Meat Fish</h2>
+            <img src={meat} alt="meat" />
+            <div className="headers">
+                <div>
+                    <h2>
+                        M
+                    <br />
+                    E
+                    <br />
+                    A
+                    <br />
+                    T
+                   </h2>
+                    <h2>
+                        F
+                    <br />
+                    I
+                    <br />
+                    S
+                    <br />
+                    H
+                   </h2>
+                </div>
+                <h2>AND</h2>
+            </div>
+            <img src={fish} alt="fish" />
         </div>
     );
 }
